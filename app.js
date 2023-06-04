29b48343f755205c631e73a9af20f8a52b02c71d
zeroboycoder <pyaesonekhant@tumawlamyine.edu.mm>
2023-06-04 19:15:29 +0630

    text color changed

--- app.js
+++ app.js
@@ -1,1 +1,18 @@
 console.log("hello world");
+
+/**
+ * git init (initialized)
+ * git add [file_name]/.
+ * git commit -m ""
+ * git status
+ * git log
+ *
+ * git remote [add/remove] [remote_name] [remote_link]
+ * git remote add orgin htttps://.....git
+ *
+ * git branch -M [branch_name]
+ * git branch -M main
+ *
+ * git push [remote_name] [branch_name]
+ * git push origin main
+ */
